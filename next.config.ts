5df947da243d6b26856a1e61abb2163be4e0c3ec
toddlerya <toddlerya@gmail.com>
2025-10-23 15:00:25 +0800

    update chapter-10

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  cacheComponents: true, // 移到顶层配置
 };
 
 export default nextConfig;
